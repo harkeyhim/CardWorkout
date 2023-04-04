05a7cdce10a90aba01fffed34d31cb7f6618aa63
Iam_Hey-k <60710389+harkeyhim@users.noreply.github.com>
2023-04-04 14:32:07 +0100

    Initial Commit

---
+++ CardWorkout/CardSelectionViewController.swift
@@ -0,0 +1,51 @@
+//
+//  CardSelectionViewController.swift
+//  CardWorkout
+//
+//  Created by Yusuf Akeem on 04/04/2023.
+//
+
+import UIKit
+
+class CardSelectionViewController: UIViewController {
+
+    @IBOutlet var cardImageView: UIImageView!
+    @IBOutlet var buttons: [UIButton]!
+    var cards: [UIImage] = Card.allValues
+    
+    
+    var timer: Timer!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startTimer()
+        for button in buttons{
+            button.layer.cornerRadius = 8
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        timer.invalidate()
+    }
+    
+    
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true )
+    }
+    @objc func showRandomImage() {
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+    }
+
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+    }
+    
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        startTimer()
+    }
+    
+    @IBAction func rulesButtonTapped(_ sender: UIButton) {
+    }
+}
